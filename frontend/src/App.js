65f24ad0a49cd82919f034b1dcd205e659f0d08c
abdullahxyz85 <abdullahxyz85@gmail.com>
2025-07-08 15:22:28 +0500

    Make Changegs in the UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
-import Header from './components/Header';
-import Home from './pages/Home';
-import ReviewPage from './pages/ReviewPage';
-import LoginPage from './pages/LoginPage';
-import './index.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import ReviewPage from "./pages/ReviewPage";
+import LoginPage from "./pages/LoginPage";
+import Features from "./pages/Features";
+import About from "./pages/About";
+import "./index.css";
 
 function App() {
   return (
@@ -13,11 +20,13 @@ function App() {
       <Router>
         <div className="min-h-screen bg-gray-50">
           <Header />
-          <main className="container mx-auto px-4 py-8">
+          <main className="container mx-auto px-4 py-2">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<LoginPage />} />
               <Route path="/review" element={<ReviewPage />} />
+              <Route path="/features" element={<Features />} />
+              <Route path="/about" element={<About />} />
               <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
           </main>
@@ -27,4 +36,4 @@ function App() {
   );
 }
 
-export default App; 
+export default App;
